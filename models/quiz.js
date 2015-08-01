d4ff5bc66da4014f457d5199bfc7b77660cbd6d8
David-PC <davepc@gmail.com>
2015-08-01 19:58:30 +0200

    Tematica

--- models/quiz.js
+++ models/quiz.js
@@ -1,16 +1,19 @@
 ﻿// Definicion del modelo de Quiz con validación
 
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define(
-  	'Quiz',
-    { pregunta: {
-        type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Pregunta"}}
-      },
-      respuesta: {
-        type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Respuesta"}}
-      }
+module.exports = function(sequelize,DataTypes) {
+  return sequelize.define('Quiz', {
+    pregunta: {
+      type: DataTypes.STRING,
+      validate: { notEmpty: {msg: 'Campos pregunta obligatorio'} }
+    },
+
+    respuesta: {
+      type: DataTypes.STRING,
+      validate: { notEmpty: {msg: 'Campos respuesta obligatorio'} }
+    },
+
+    tema: {
+      type: DataTypes.STRING
     }
-  );
+  });
 }
