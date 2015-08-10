6c97a15770dadc75a5f6794a2116de8e52e64a16
David-PC <davepc@gmail.com>
2015-08-10 18:01:24 +0200

    Autenticacion de usuarios

--- models/quiz.js
+++ models/quiz.js
@@ -1,19 +1,16 @@
 ﻿// Definicion del modelo de Quiz con validación
 
-module.exports = function(sequelize,DataTypes) {
-  return sequelize.define('Quiz', {
-    pregunta: {
-      type: DataTypes.STRING,
-      validate: { notEmpty: {msg: 'Campos pregunta obligatorio'} }
-    },
-
-    respuesta: {
-      type: DataTypes.STRING,
-      validate: { notEmpty: {msg: 'Campos respuesta obligatorio'} }
-    },
-
-    tema: {
-      type: DataTypes.STRING
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define(
+  	'Quiz',
+    { pregunta: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "-> Falta Pregunta"}}
+      },
+      respuesta: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+      }
     }
-  });
+  );
 }
